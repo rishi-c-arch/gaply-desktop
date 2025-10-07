ef758b491f1ee1142662a9d7cb49441d9a3da43a
RishiSTARP <rishirajsharma8055@gmail.com>
2025-10-08 04:01:54 +0530

    Test simple 3D component to verify Three.js is working

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import PremiumHeroText from './components/PremiumHeroText';
 import HeroToSecondTransition from './components/HeroToSecondTransition';
 import FreeFeatures3D from './components/FreeFeatures3D';
 import QuartileAnalysis3D from './components/QuartileAnalysis3D';
-import Interactive3DGraph from './components/Interactive3DGraph';
+import Simple3DTest from './components/Simple3DTest';
 import PremiumFooter3D from './components/PremiumFooter3D';
 import PremiumPage from './components/PremiumPage';
 import UserDashboard from './components/UserDashboard';
@@ -312,7 +312,7 @@ const App: React.FC = () => {
             <HeroSection />
             <FreeFeatures3D />
             <HeroToSecondTransition />
-            <Interactive3DGraph />
+            <Simple3DTest />
             <QuartileAnalysis3D />
             <PremiumFooter3D />
           </>
